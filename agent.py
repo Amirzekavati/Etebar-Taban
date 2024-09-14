cc3446d66457780d91b7f0f6b40702f967772763
Amirz <amir.zekavati.a@gmail.com>
2024-09-14 01:52:17 -0700

    The tested form of code

---
+++ agent.py
@@ -0,0 +1,109 @@
+from database import AgentDataBase
+import requests
+from datetime import datetime, timedelta
+import time
+
+class CommissionAgent:
+
+    def __init__(self):
+        # create database
+        self.db = AgentDataBase()
+
+    def get_records(self, fromDate_str, toDate_str, page=1):
+        fromDate = datetime.strptime(fromDate_str, "%Y-%m-%d")
+        toDate = datetime.strptime(toDate_str, "%Y-%m-%d")
+
+        # set URL and specification
+        url = f"https://tbsapi.etbrokerage.ir/api/CustomerClub/GetCustomerTotalBrokerCommission?fromDate={fromDate_str}&toDate={fromDate_str}&page={page}&pageSize=100"
+        username = "Tahlil.taban"
+        password = "Et@@2025MR"
+
+        # set THE header
+        headers = {
+            "UserName": username,
+            "Password": password
+        }
+
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        while fromDate.date() <= toDate.date():
+            while len(data["Result"]) != 0:
+                for record in data["Result"]:
+                    commission = {
+                        "OnlineSellCommission": float(record["OnlineSellCommission"]),
+                        "OnlineBuyCommission": float(record["OnlineBuyCommission"]),
+                        "NonOnlineSellCommission": float(record["NonOnlineSellCommission"]),
+                        "NonOnlineBuyCommission": float(record["NonOnlineBuyCommission"]),
+                        "TotalCommission": float(record["OnlineSellCommission"])
+                                           + float(record["OnlineBuyCommission"])
+                                           + float(record["NonOnlineSellCommission"])
+                                           + float(record["NonOnlineBuyCommission"]),
+                        "AccountCode": record["AccountCode"],
+                        "Date": record["Date"]
+                    }
+                    print(fromDate.date())
+                    self.db.upsert(commission)
+
+                page += 1
+                url = f"https://tbsapi.etbrokerage.ir/api/CustomerClub/GetCustomerTotalBrokerCommission?fromDate={str(fromDate.date())}&toDate={str(fromDate.date())}&page={page}&pageSize=100"
+                response = requests.get(url, headers=headers)
+                data = response.json()
+                
+            page = 1
+            fromDate += timedelta(days=1)
+            url = f"https://tbsapi.etbrokerage.ir/api/CustomerClub/GetCustomerTotalBrokerCommission?fromDate={str(fromDate.date())}&toDate={str(fromDate.date())}&page={page}&pageSize=100"
+            response = requests.get(url, headers=headers)
+            data = response.json()
+            print(fromDate.date())
+
+    def total_commission_customer(fromDate_str, toDate_str):
+        fromDate = datetime.strptime(fromDate_str, "%Y-%m-%d")
+        toDate = datetime.strptime(toDate_str, "%Y-%m-%d")
+        
+        # set URL and specification
+        url = f"https://tbsapi.etbrokerage.ir/api/CustomerClub/GetCustomerTotalBrokerCommission?fromDate={fromDate_str}&toDate={fromDate_str}&page={page}&pageSize=100"
+        username = "Tahlil.taban"
+        password = "Et@@2025MR"
+
+        # set THE header
+        headers = {
+            "UserName": username,
+            "Password": password
+        }
+
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        
+        while fromDate.date() <= toDate.date():
+            while len(data["Result"]) != 0:
+                for record in data["Result"]:
+                    commission = {
+                        "TotalCommission": float(record["OnlineSellCommission"])
+                                           + float(record["OnlineBuyCommission"])
+                                           + float(record["NonOnlineSellCommission"])
+                                           + float(record["NonOnlineBuyCommission"]),
+                        "AccountCode": record["AccountCode"],
+                    }
+                    
+                    self.db.update(commission)
+
+                page += 1
+                url = f"https://tbsapi.etbrokerage.ir/api/CustomerClub/GetCustomerTotalBrokerCommission?fromDate={str(fromDate.date())}&toDate={str(fromDate.date())}&page={page}&pageSize=100"
+                response = requests.get(url, headers=headers)
+                data = response.json()
+                
+            page = 1
+            fromDate += timedelta(days=1)
+            url = f"https://tbsapi.etbrokerage.ir/api/CustomerClub/GetCustomerTotalBrokerCommission?fromDate={str(fromDate.date())}&toDate={str(fromDate.date())}&page={page}&pageSize=100"
+            response = requests.get(url, headers=headers)
+            data = response.json()
+            print(fromDate.date())
+
+        
+        
+        
+        
+if __name__ == "__main__":
+    agent = CommissionAgent()
+    # agent.get_records("2024-06-30", "2024-09-09")
+    agent.total_commission_customer("2024-09-09", "2024-09-14")
---
+++ database.py
@@ -0,0 +1,64 @@
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+import time
+
+class AgentDataBase:
+    def __init__(self, client='mongodb://localhost:27017/', db_name='EtebarTaban', collection_name='specificationsPerDay'):
+        # initialize database
+        self.client = MongoClient(client)
+        self.database = self.client[db_name]
+        self.collection = self.database[collection_name]
+        
+    def update(self, message_dict, collection_name='Commission'):
+        collection = self.database[collection_name]
+        existing_doc = self.collection.find_one({'AccountCode':message_dict['AccountCode']})
+        if existing_doc:
+            collection.update_one({'TotalCommission': existing_doc['TotalCommission']}, existing_doc['TotalCommission'] + message_dict['TotalCommission'])
+            print("update the commission")
+        else:
+            collection.insert_one(message_dict)
+            print("insert")
+    # for insert document into database
+    # if document exist replace into database
+    def upsert(self, message_dict):
+        existing_doc = self.collection.find_one({'AccountCode': message_dict['AccountCode'],
+                                                 'Date': message_dict['Date']})
+        # if we have disticnt data then replace
+        if existing_doc:
+            self.collection.replace_one({'AccountCode': existing_doc['AccountCode']}, message_dict)
+            # print(existing_doc['AccountCode'])
+            # time.sleep(2)
+            print("replace the message")
+        else:
+            # print(message_dict['AccountCode'])
+            # time.sleep(2)
+            self.collection.insert_one(message_dict)
+            print("insert the message")
+
+    # delete document
+    def delete(self, message_dict):
+        self.collection.delete_one(message_dict)
+        print("The message was deleted successfully")
+
+    # close database
+    def close(self):
+        if self.client:
+            self.client.close()
+            self.client = None
+        print("The database was closed")
+
+    # remove all of documents
+    def clear_database(self):
+        self.collection.delete_many({})
+        print("clear the database")
+
+    def check_connection(self):
+        try:
+            server_status = self.database.command("serverStatus")
+            print("The connection is successful")
+            return {"Status": "success", "Message": "Database connected successfully",
+                    "Server_host": server_status["host"]}
+        except PyMongoError as e:
+            return {"status": "error", "Message": f"Database connection failed: {e}"}
+        except Exception as e:
+            return {"status": "error", "Message": f"Database error: {e}"}
